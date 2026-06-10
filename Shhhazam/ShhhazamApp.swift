80cf119398634de145fe739cf81cfbe1f2cb31ab
Dave Harris <dave@harris.org.nz>
2026-06-10 12:42:47 +1200

    Initial Commit

---
+++ Shhhazam/ShhhazamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShhhazamApp.swift
+//  Shhhazam
+//
+//  Created by Dave Harris on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShhhazamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
